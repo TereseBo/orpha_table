e2b4bb169f788eed34492cef1fd7454ce9a837da
TereseBo~ <terese.boderus@yh.nackademin.se>
2024-07-09 09:02:51 +0200

    Updated routes for icd10

--- app/api/fetchorphainfo.js
+++ app/api/fetchorphainfo.js
@@ -7,75 +7,73 @@ export const fetchJson = async (url, init = {}) => {
     return res.json();
 };
 
-// fetches ICD-10, and all names from RD-CODE API by code
+// fetches ICD-10, and all synonyms from RD-CODE API by code
 export async function fetchOrphaInfo(code) {
-
     const options = {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
             apikey: process.env.ORPHA_API_KEY,
-
         },
-    }
+    };
 
     return Promise.all([
-
-        fetchJson(`https://api.orphacode.org/EN/ClinicalEntity/orphacode/${code}/ICD10`,
-            { ...options }
-        ),
-        fetchJson(`https://api.orphacode.org/EN/ClinicalEntity/orphacode/${code}/Synonym`,
-            { ...options }
-
-        ),
+        fetchJson(`https://api.orphacode.org/EN/ClinicalEntity/orphacode/${code}/ICD10`, { ...options }),
+        fetchJson(`https://api.orphacode.org/EN/ClinicalEntity/orphacode/${code}/Synonym`, { ...options }),
     ]).then((values) => {
-
-        const disease = {}
-        let reducedData = values.reduce((accumulator, currentValue) => Object.assign({}, accumulator, currentValue))
-        disease.referencesICD10 = reducedData.References.map(item => item["Code ICD10"]) || ["-"]
-        disease.orphacode = reducedData.ORPHAcode
-        disease.preferredTerm = reducedData["Preferred term"] || ["-"]
-        disease.synonyms = reducedData.Synonym || ["-"]
-        return [disease]
+        const disease = {};
+        let reducedData = values.reduce((accumulator, currentValue) => Object.assign({}, accumulator, currentValue));
+        disease.referencesICD10 = reducedData.References.map(item => item["Code ICD10"]) || ["-"];
+        disease.orphacode = reducedData.ORPHAcode;
+        disease.preferredTerm = reducedData["Preferred term"] || ["-"];
+        disease.synonyms = reducedData.Synonym || ["-"];
+        return [disease];
     });
 };
 
-// fetches ICD-10, and all names from RD-CODE API by code
+
 export async function fetchSynonyms(diseaseData) {
-    console.log('in fetch synonyms ')
-
-
     const options = {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
             apikey: process.env.ORPHA_API_KEY,
-
         },
-    }
+    };
 
     const apiCalls = diseaseData.map(disease => {
-        return fetchJson(`https://api.orphacode.org/EN/ClinicalEntity/orphacode/${disease.orphacode}/Synonym`,
-            { ...options }
-
-        )
-    })
-
+        return fetchJson(`https://api.orphacode.org/EN/ClinicalEntity/orphacode/${disease.orphacode}/Synonym`, { ...options });
+    });
 
     return Promise.all(apiCalls).then((values) => {
-        console.log("In  fetch synonyms, VALUES")
-       console.log(values)
-       console.log("In  fetch synonyms, DISEASEDATA")
-       console.log(diseaseData)
-       console.log('The type check should be here')
-       Array.isArray(diseaseData)
-        diseaseData.array.forEach(element => {
-            const matchedItem = values.find(item => item.ORPHAcode === element.orphacode)
-            element.synonyms = matchedItem.Synonym || ['-']
-
+        diseaseData.forEach((disease, index) => {
+            const synonymsData = values[index];
+            disease.synonyms = synonymsData?.Synonym?.length > 0 ? synonymsData.Synonym : ['-'];
         });
-        console.log(diseaseData)
-        return diseaseData
-    })
+        return diseaseData;
+    });
 }
 
+export async function fetchICD10Info(icd10) {
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            apikey: process.env.ORPHA_API_KEY,
+        },
+    };
+
+    const diseaseData = await fetchJson(`https://api.orphacode.org/EN/ClinicalEntity/ICD10/${icd10}`, { ...options })
+        .then((values) => {
+            return values.References.map(icd10Disease => {
+                return {
+                    orphacode: icd10Disease.ORPHAcode,
+                    preferredTerm: icd10Disease["Preferred term"] || "-",
+                    referencesICD10: [icd10Disease.ICD],
+                };
+            });
+        });
+
+    const completeDiseaseData = await fetchSynonyms(diseaseData);
+    return completeDiseaseData;
+}
--- app/api/icd10/[term]/route.js
+++ app/api/icd10/[term]/route.js
@@ -1,63 +1,16 @@
 import { NextResponse } from 'next/server';
 import { fetchJson, fetchSynonyms } from '../../fetchorphainfo';
-import { fetchOrphaInfo } from '../../fetchorphainfo';
+import { fetchICD10Info } from '../../fetchorphainfo';
+
 
 
 export async function GET(req, { params }) {
-  console.log("GET in /api/icd10/term")
-  const icd10 = params.term
-  console.log("icd10: ", icd10)
+  const icd10 = params.term;
+
   try {
-    // Fetch the list of diseases associated with the icdCode
-    const options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        apikey: process.env.ORPHA_API_KEY,
-
-      },
-    }
-    const diseaseData = await fetchJson(`https://api.orphacode.org/EN/ClinicalEntity/ICD10/${icd10}`, { ...options }).then((values) => {
-      console.log("in then values")
-      // console.log("values: ", values.References)
-      let icd10Diseases = values.References.map(icd10Disease => {
-        let disease = {}
-        disease.orphacode = icd10Disease.ORPHAcode
-        disease.preferredTerm = icd10Disease["Preferred term"] || ["-"]
-        disease.referencesICD10 = [icd10Disease.ICD]
-        disease.synonyms = ['-']
-
-        return disease
-      })
-      // console.log(icd10Diseases)
-
-
-      return icd10Diseases
-    });
-
-    const completeDiseaseData = await diseaseData.map(async disease => {
-      console.log('diseasse')
-      console.log(disease)
-       fetchJson(`https://api.orphacode.org/EN/ClinicalEntity/orphacode/${disease.orphacode}/Synonym`,
-        { ...options }).then(res => {
-          console.log('res.')
-          console.log(res)
-          if (res.Synonym) {
-            disease.synonyms = res.Synonym
-          }else{
-            disease.synonyms=['-']
-          }
-          const completeDisease = { ...disease }
-       console.log('completedisease')
-       console.log(completeDisease)
-          return completeDisease
-        })
-
-    })
-    console.log('completeDiseaseData')
-    console.log(completeDiseaseData)
+    const diseaseData = await fetchICD10Info(icd10);
     return new NextResponse(
-      JSON.stringify(completeDiseaseData),
+      JSON.stringify(diseaseData),
       { status: 200 }
     );
   } catch (error) {
@@ -68,3 +21,4 @@ export async function GET(req, { params }) {
     );
   }
 }
+
