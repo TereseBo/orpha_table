773b57116a75818e3601752235b9174030818ce2
TereseBo~ <terese.boderus@yh.nackademin.se>
2024-07-16 10:38:58 +0200

    Added toast for errors and validation

--- app/page.js
+++ app/page.js
@@ -1,17 +1,19 @@
 "use client"
 
-import { SearchBox } from "@/components/searchbox";
-import React from 'react';
-import { ResultsTable } from "@/components/result/resultstable";
-import { SelectionTable } from "@/components/selection/selectiontable";
+import { SearchBox } from "@/components/searchbox"
+import React from 'react'
+import { ResultsTable } from "@/components/result/resultstable"
+import { SelectionTable } from "@/components/selection/selectiontable"
+import { Toaster } from 'react-hot-toast'
 
 export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-8 gap-4">
+      <Toaster />
       <SearchBox />
-        <ResultsTable />
-        <SelectionTable/>
+      <ResultsTable />
+      <SelectionTable />
 
     </main>
   );
