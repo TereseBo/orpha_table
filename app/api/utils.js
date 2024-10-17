a2552a5350abf3c5026d6909e78f7c02c26e6582
TereseBo~ <terese.boderus@yh.nackademin.se>
2024-10-17 09:22:28 +0200

    Added function to fetch icd11 by orphacode

--- app/api/utils.js
+++ app/api/utils.js
@@ -49,6 +49,46 @@ export async function fetchICD10Codes(diseaseData) {
     });
 }
 
+// fetches ICD-10 by ORPHAcode for disease objects in an array
+export async function fetchICD11Codes(diseaseData) {
+
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            apikey: process.env.ORPHA_API_KEY,
+        },
+    };
+
+    //Create an array of fetch calls including all disease 
+    const apiCalls = diseaseData.map(disease => {
+        return fetchJson(`https://api.orphacode.org/EN/ClinicalEntity/orphacode/${disease.orphacode}/ICD11`, { ...options })
+            .catch(error => {
+                if (error.message.includes('404')) {
+                    return { ORPHAcode: disease.orphacode, References: [] };  // Fallback for missing ICD-11 codes
+                }else{
+                    return { ORPHAcode: disease.orphacode, References: [{"Code ICD11":"Something went wrong"}] };  // Fallback for failed fetches
+                }
+            });
+    });
+
+    return Promise.all(apiCalls).then((values) => {
+
+        const icd11Data = []
+        values.map(value => {
+
+            icd11Data.push({
+                orphacode: value.ORPHAcode, referencesICD11: Array.isArray(value.References) && value.References.length > 0
+                    ? value.References.map(ref => ref["Code ICD11"])
+                    : ['-']
+            })
+        })
+
+
+        return icd11Data;
+    });
+}
+
 
 
 //Fetches synonyms by ORPHAcode for disease objects in an array
