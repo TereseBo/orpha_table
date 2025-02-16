5e5fb88b88e7ce4c768df1be0e9b5453ff2c7131
TereseBo <terese.boderus@gmail.com>
2025-02-16 09:08:52 +0100

    Frontend for displaying results from search by list

---
+++ components/list/listresulttable.jsx
@@ -0,0 +1,17 @@
+import { DataTable } from "@/components/ui/datatable";
+import { columns } from "./columns";
+
+import React from 'react';
+import useStore from '@/zustandstore/orphastore';
+
+export function ListResultTable() {
+
+    const searchResultList = useStore((state) => state.searchResultList);
+    
+    return (
+        <div className="m-2 w-5/6">
+            <h1>Search results</h1>
+            <DataTable columns={columns} data={searchResultList} />
+        </div>
+    )
+}
