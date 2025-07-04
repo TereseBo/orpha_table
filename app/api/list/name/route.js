200ac36e4a12b24871e0d616f5fabceec00afb07
TereseBo <terese.boderus@gmail.com>
2025-07-04 09:53:13 +0200

    Added new route and functions for list name search

---
+++ app/api/list/name/route.js
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server';
+import { fetchORPHAcodesByName } from '@/app/api/list'
+
+export async function POST(req, { params }) {
+
+    try {
+        const body = await req.json();
+
+        let indata = body.values
+        let namecolumn = body.column - 1
+        let headerRow = body.headerRow
+        let inDataArray = Array.from([...indata])
+
+        const diseaseData = await fetchORPHAcodesByName(inDataArray, namecolumn, headerRow);
+ 
+        if (diseaseData.length === 1) {
+            return new NextResponse(
+
+                JSON.stringify({ message: `No data to return, please verify your inputs` }),
+                { status: 404 }
+            );
+        }
+        return new NextResponse(
+            JSON.stringify(diseaseData),
+            { status: 200 }
+        );
+    } catch (error) {
+
+        if (error.message.includes('413')) {
+            return new NextResponse(
+                JSON.stringify({ message: `To many results for list, please choose another search method or split your list` }),
+                { status: 413 }
+            );
+        }
+        return new NextResponse(
+            JSON.stringify({ message: 'Something went wrong when getting the orphacodes, please try again later' }),
+            { status: 500 }
+        );
+    }
+}
